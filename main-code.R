34cb1f679a33e7c782c9a213b2bfebb5f5b81969
Wonday Seyoum <wondwosen30@gmail.com>
2022-12-01 22:25:10 -0600

    update2

--- main-code.R
+++ main-code.R
@@ -3,13 +3,25 @@ library(lubridate)
 
 dataset1 <- read_csv("dataset/site9991799.csv")
 
-dataset1$Date <- mdy(dataset1$Date)
+#import the second dataset and combine it here first
+
 
 metaData <- read_csv("dataset/locations_metadata.txt")
 
-#Lakes with no names in the metadata, name was assigned with NM and Pour_lat value
-
+#Add lake name to the reluctance data
+##Lakes with no names in the metadata, name was assigned with Pour_lat value
 metaData <- metaData %>% 
   mutate(Lake_name = coalesce(Lake_name, as.character(Pour_lat)))
 
-#Add lake name to the reflectance data
+fidLakeName <- metaData %>% 
+  select(FID, Lake_name) 
+
+dataset1 <- left_join(dataset1, fidLakeName, by=c("ID"="FID"))
+
+#aggregate to annual average
+dataset1$Date <- mdy(dataset1$Date)
+dataset1$Year <- year(dataset1$Date)
+
+#Decided to calculate color DW first and then aggregate per year annual
+
+
